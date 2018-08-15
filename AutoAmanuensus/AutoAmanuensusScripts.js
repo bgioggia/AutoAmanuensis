53fc4509a50049a738fa01fecbb32f5b62aa9b56
Brandon Gioggia <bgioggia@gmail.com>
2018-08-14 20:33:53 -0400

    Add files via upload
    *VARIABLES ADDED* 
    The 'synonymDict' variale is used to store the objects returned by '$getWords' so they may be later used by the 'getSynonyms' function.
    
    The 'finalOutput' variable is the final string that is created as output to tell the user the words that have been repeated, how many times they have been repeated, and their synonyms.
    
    *FUNCTIONS ADDED*
    The 'getSynonyms' function assigns the values of synonyms from the synonymDict values, organizes them in the 'finalOutput' string. The format shows the word, number of time it is repeated, and the respective synonyms for nouns, verbs, and adjectives. The 'EssayOutput' value is then printed to the EssayOutput box.
    
    *FUNCTIONS CHANGED*
    'getNouns', 'getVerbs', and 'getAdjectives' all have been changed to prevent undefined words from causing errors when the program runs.
    
    *Cosmetic Changes*
    The text boxes and buttons have been centered, and the background color has been changed.

--- AutoAmanuensus/AutoAmanuensusScripts.js
+++ AutoAmanuensus/AutoAmanuensusScripts.js
@@ -8,24 +8,38 @@ var dict = {};
 //The 'maxRepeat' variable is a number that is used as a reference by functions to determine how many times a word may be repeated before a suggestion for a replacment is provided.
 var maxRepeat = 0;
 
-//The 'commonWords' variable is an array containing common words that will be overlooked when suggesting synonyms.
-var commonWords = ["i", "a", "and", "but", "if", "when", "where", "how", "", " ", ];
+//The 'commonWords' variable is an array containing common words that 
+//will be overlooked when suggesting synonyms.
+var commonWords = ["the", "what","up","that", "youre", "its", "it", "your", "were", "my", "here", "im", "are", "you", "for", "this", "be", "to", "dont", "we", "in","if", "of", "i", "a", "and", "but", "if", "when", "where", "how", "", " ", "or", "why", "is"];
 
-//The 'flaggedWords' variabe is an array containing all words that have been flagged as exceeding the maximum number of repeats for the current essay. 
+//The 'flaggedWords' variabe is an array containing all words that have been
+//flagged as exceeding the maximum number of repeats for the current essay. 
 var flaggedWords = [];
 
-//
+//The 'essay' variable is used to store essay that is input by the user.
 var essay = "";
 
-//The 'repeatedWords '
+//The 'repeatedWords' variable is used to store the elements of 'flaggedWords' as a string for printing 
 var repeatedWords = "";
+
+//The 'synonymDict' variale is used to store the objects returned by '$getWords' so they may be later used
+//by the 'getSynonyms' function.
+var synonymDict = {};
+
+//The 'finalOutput' variable is the final string that is created as output to tell the user the words
+//that have been repeated, how many times they have been repeated, and their synonyms.
+var finalOutput = ""
 
 /*
 JAVASCRIPT FUNCTIONS
 */
 
-//
-//
+//The 'setEssayVal' function is run when the check Repeats button is pressed. The function
+//runs the 'setMaxRepeat' function, setting the maximum value based on what the user has entered.
+//The function then value of the 'essay' variable to the text that the user has entered.
+//The function then runs this value through the compileWordsRunner and WordRepeatChecker; the function 
+//prints the words that are repeated mroe than the maximum allowed amount in the OutputBox.
+//The 'synonymDict' object is then filled with the synonym objects of the flagged words.
 function setEssayVal()
 {
 	setMaxRepeat();
@@ -34,6 +48,29 @@ function setEssayVal()
 	compileWordsRunner(essay);
 	wordRepeatChecker();
 	document.getElementById("EssayOutput").value = "The following words are repeated more than " + maxRepeat + " times:\n\n" + flaggedWordsToString();
+
+	for (x in flaggedWords)
+		{
+			synonymDict[flaggedWords[x]] = $getWords(flaggedWords[x]);
+		}
+}
+
+//The 'getSynonyms' function assigns the values of synonyms from the synonymDict values, organizes
+//them in the 'finalOutput' string. The format shows the word, number of time it is reapeated,
+//and the respective synonyms for nouns, verbs, and adjectives. The 'EssayOutput' value is then printed
+//to the EssayOutput box.
+function getSynonyms()
+{	
+	finalOutput = "";
+	for (x in synonymDict)
+	{
+		finalOutput = finalOutput + "Word: " + x + "\nRepeats: " + dict[x] + "\n";
+		finalOutput = finalOutput  + "Noun Synonyms: " + getNouns(synonymDict[x]) + "\n";
+		finalOutput = finalOutput  +"Verb Synonyms: " + getVerbs(synonymDict[x]) + "\n";
+		finalOutput = finalOutput +"Adjective Synonyms: " + getAdjectives(synonymDict[x]) + "\n" + "\n" +"\n";
+	}
+	document.getElementById("EssayOutput").value = finalOutput;
+	synonymDict = {};
 }
 
 //The 'setMaxRepeat' function takes a number as an input and sets the 'maxRepeat' variable equal to it. If a number is not entered, the function does nothing.
@@ -48,6 +85,7 @@ function setMaxRepeat(num)
 function $getWords(word)
 {
 	var data = "http://words.bighugelabs.com/api/2/de09a1667a75c2330105a64129c9db42/" + word + "/json";
+
 	return $.getJSON(data);
 }
 
@@ -55,21 +93,55 @@ function $getWords(word)
 //JSON -> Array of Strings
 function getNouns(JSON)
 {
-  return JSON.responseJSON.noun.syn
+	if ((typeof JSON.responseJSON == "undefined") || (typeof JSON.responseJSON.noun == "undefined")|| (typeof JSON.responseJSON.noun.syn == "undefined"))
+		return "None";
+
+	var nounString = "";
+	var temp = JSON.responseJSON.noun.syn;
+
+	for (i=0; i < 10; i++)
+	{
+		if(typeof temp[i] =="undefined")
+			return nounString.substring(1, nounString.length);
+		nounString = nounString + ", " + temp[i];
+	}
+	return nounString.substring(1, nounString.length);
 }
 
 //The 'getVerbs' function takes the output given by the ____ API in the $getWords function, and will return an array of all Verb Synonyms that are present. 
 //JSON -> Array of Strings
 function getVerbs(JSON)
 {
-  return JSON.responseJSON.verb.syn
+	if ((typeof JSON.responseJSON == "undefined") || (typeof JSON.responseJSON.verb == "undefined")|| (typeof JSON.responseJSON.verb.syn  == "undefined"))
+		return "None";
+	var verbString = "";
+	var temp = JSON.responseJSON.verb.syn;
+	
+	for (i=0; i < 10; i++)
+	{
+		if(typeof temp[i] =="undefined")
+			return verbString.substring(1, verbString.length);
+		verbString = verbString + ", " + temp[i];
+	}
+	return verbString.substring(1, verbString.length);
 }
 
 //The 'getAdjectives' function takes the output given by the ____ API in the $getWords function, and will return an array of all Adjective Synonyms that are present. 
 //JSON -> Array of Strings
 function getAdjectives(JSON)
 {
-  return JSON.responseJSON.adjective.syn
+	if ((typeof JSON.responseJSON == "undefined") || (typeof JSON.responseJSON.adjective == "undefined")|| (typeof JSON.responseJSON.adjective.syn == "undefined"))
+		return "None";
+	var adjectiveString = "";
+	var temp = JSON.responseJSON.adjective.syn;
+	
+	for (i=0; i < 10; i++)
+	{
+		if(typeof temp[i] =="undefined")
+			return adjectiveString.substring(1, adjectiveString.length);
+		adjectiveString = adjectiveString + ", " + temp[i];
+	}
+	return adjectiveString.substring(1, adjectiveString.length);
 }
 
 //The 'wordRepeatChecker' function checks the dictionary to see what words have been repeated more times than the maximum value allotted by 'maxRepeat', and stores them in an array of strings.
@@ -79,7 +151,7 @@ function wordRepeatChecker()
 	flaggedWords = [];
   for (x in dict)
   {
-    if(!(commonWords.includes(x)) && (dict[x]> maxRepeat))
+    if(!(commonWords.includes(x)) && (dict[x] >= maxRepeat))
     {
       flaggedWords.push(x);
   }
@@ -87,36 +159,31 @@ function wordRepeatChecker()
   return 0;
 }
 
-//'returnValues' is a function used for quick testing in console that returns all values that may be relevant for troubleshooting.
-//Values of Variables -> console message
-function returnValues()
-{
-  console.log("dict Values");
-  console.log(dict);
-  console.log();
-  console.log("maxRepeat Value");
-  console.log(maxRepeat);
-  console.log();
-  console.log("flaggedWords Values");
-  console.log(flaggedWords);
-}
-
 //'compileWordsRunner' takes in an essay in the form of a String, and places all of the words in order into an array called wordList. It the nuses helper functions to determine the number of times each word repeats.
 //String -> [Change Values in 'dict']
 function compileWordsRunner(essay)
 {
 	essay = essay.replace(/\n/ig," ");
-	console.log(essay);
+
   //splits up input into array where each word is an index
   var wordList = essay.split(" ");
-  console.log(wordList);
-  //var temp = ""
-  //temp = 
 
   //for loop designed to make all words in the array completely lowercase
   var x;
   for (x in wordList)
   {
+  	wordList[x] = wordList[x].replace(',','');
+  	wordList[x] = wordList[x].replace('.','');
+  	wordList[x] = wordList[x].replace('(','');
+  	wordList[x] = wordList[x].replace('\\','');
+  	wordList[x] = wordList[x].replace('/','');
+  	wordList[x] = wordList[x].replace(')','');
+  	wordList[x] = wordList[x].replace(';','');
+  	wordList[x] = wordList[x].replace('\'','');
+  	wordList[x] = wordList[x].replace('-','');
+  	wordList[x] = wordList[x].replace('?','');
+  	wordList[x] = wordList[x].replace('!','');
+  	wordList[x] = wordList[x].replace(':','');
     wordList[x] = wordList[x].toLowerCase();
   }
 
@@ -159,7 +226,8 @@ function endOfArray(wordList)
     }
 }
 
-//
+//The 'flaggedWordsToString' function is designed to take the values from 'flaggedWords' and save
+//them to 'repeatedWords' separated by commas.
 //[Value of 'flaggedWords'] -> String
 function flaggedWordsToString()
 {
